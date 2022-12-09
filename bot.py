f1f47d6cd306d3ff95cef2c01f57e79a677d0b36
Akapana 2140 <tangaferoz3@gmail.com>
2022-12-08 23:08:53 -0500

    segund commit

--- bot.py
+++ bot.py
@@ -2,44 +2,19 @@
 from lnm_class import Trading as tr
 import tracker as tracker
 import time
+import tradingview_ta 
+from tradingview_ta import TA_Handler, Interval, Exchange
 
 
 def bot():
 
-    while True:
-        DataPrice = open("dataPrice.csv","r")             
-        lastline = DataPrice.readlines()[-1].split(",")   
-        DataPrice.close()                                  
-        priceIndex = lastline[0]                           
-        priceBid = lastline[1]
-        priceOffer = lastline[2]
-        changeindex = float(priceIndex) * 0.005
-        indexlimitUp = changeindex + float(priceIndex)
-        indexlimitDown = float(priceIndex) - changeindex
-
-        if float(priceIndex) >= float(indexlimitUp):
-            print("AHORA!!!!!!")
-            
-        print(f"limitdown{indexlimitDown}")
-        print(f"limitUp{indexlimitUp}")
-        print(f"index{priceIndex}")
-        time.sleep(240)
-
-        
-index = tracker.index
-bid = tracker.bid
-offer = tracker.offer
-
-def rsi():
-    pass
-
-def bollinger():
-    pass
-
-def trade():
-
-    if bollinger == 1:
-        tr.long_tp_sl
-
-    elif bollinger == 0:
-        tr.Short_tp_sl
+    bitcoin = TA_Handler(
+    symbol="XBTUSDTU22",
+    screener="crypto",
+    exchange="BITMEX",
+    interval=Interval.INTERVAL_15_MINUTES,
+    )
+    analysis = bitcoin.get_analysis()
+    print(analysis)
+    
+bot()
--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import bot
 import tracker as tracker
 
 if __name__=="__main__":
-    tracker.price_scrapper()
+    
     bot.bot()
