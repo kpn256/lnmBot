17b99fbed46cfe001cf58cf1ade3f889616a884a
Akapana Monsalve <tangaferoz@proton.me>
2022-12-29 12:10:55 -0500

    same

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
-key = "pmyxPkmsV3M3bbLmt+bWulUw3KIEMgGS//mD7wYl6/U="
-secret = "/fgFpBW3ig24kUZAoJL3s72nY6jmJPaqmyRCdIDd8TLMOuB5G6Av3aK3QHi1+B1gzK6QTic9OUDTde78VEoSbA=="
-passphrase = "6ceg0e98c1ebe"
+key = ""
+secret = ""
+passphrase = ""
 options = {'key': f'{key}',
            'secret': f'{secret}',
            'passphrase': f'{passphrase}'}
